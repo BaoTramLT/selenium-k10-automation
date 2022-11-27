e45f5b74b1794b1de65b38e9fc30c89888698009
BaoTramLT <tinalee0402@gmail.com>
2022-11-27 20:36:01 +0700

    lesson 23

--- src/test/java/models/components/order/ComputerEssentialComponent.java
+++ src/test/java/models/components/order/ComputerEssentialComponent.java
@@ -4,14 +4,13 @@ import models.components.Component;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class ComputerEssentialComponent extends Component {
+public abstract class ComputerEssentialComponent extends Component {
 
     public ComputerEssentialComponent(WebDriver driver, WebElement component) {
         super(driver, component);
     }
 
-    public abstract String selectProcessorType () {
-
-    }
+    public abstract String selectProcessorType (String type);
+    public abstract String selectRAMType (String type);
 
 }
---
+++ src/test/java/tests/computer/BuyingStandardComputerTest.java
@@ -0,0 +1,21 @@
+package tests.computer;
+
+import models.components.order.CheapComputerComponent;
+import models.components.order.StandardComputerComponent;
+import org.testng.annotations.Test;
+import test_flows.computer.OrderComputerFlow;
+import tests.testng.BaseTest;
+import url.Urls;
+
+public class BuyingStandardComputerTest extends BaseTest implements Urls {
+
+    @Test
+    public void testCheapComputerBuying() {
+        driver.get(BASE_URL.concat("/build-your-own-computer"));
+
+        OrderComputerFlow<StandardComputerComponent> orderComputerFlow =
+                new OrderComputerFlow<>(driver, StandardComputerComponent.class);
+
+        orderComputerFlow.buildCompSpecAndAddToCart();
+    }
+}
