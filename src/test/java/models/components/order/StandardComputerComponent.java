faf2697a21a73dc625ce5ead2275d5d2a612555d
BaoTramLT <tinalee0402@gmail.com>
2022-11-28 20:21:02 +0700

    lesson 23

--- src/test/java/models/components/order/StandardComputerComponent.java
+++ src/test/java/models/components/order/StandardComputerComponent.java
@@ -1,18 +1,37 @@
 package models.components.order;
 
 import models.components.ComponentCssSelector;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 @ComponentCssSelector(".product-essential")
 public class StandardComputerComponent extends ComputerEssentialComponent {
+
+    private static final By productAttributeSel = By.cssSelector("select[id^=\"product_attribute\"]");
     public StandardComputerComponent(WebDriver driver, WebElement component) {
         super(driver, component);
     }
 
     @Override
     public String selectProcessorType(String type) {
-        return null;
+        final int PROCESSOR_DROPDOWN_INDEX = 0;
+        WebElement processorDropdownElem = component.findElements(productAttributeSel).get(PROCESSOR_DROPDOWN_INDEX);
+        Select select = new Select(processorDropdownElem);
+        List<WebElement> allOptions = select.getOptions();
+        String fullStrOption = null;
+
+        for (WebElement option : allOptions) {
+            String currentOptionText = option.getText();
+            String optionTextWithoutSpaces = currentOptionText.trim().replace(" ", "");
+            if (optionTextWithoutSpaces.startsWith(type)) {
+                fullStrOption = currentOptionText;
+                break;
+            }
+        }
     }
 
     @Override
